f02d017ba0ede247badee12eed25eb51e5a6fc77
fatimaalojaimi <45715105+fatimaalojaimi@users.noreply.github.com>
2022-12-12 10:28:20 +0300

    add orer classes

---
+++ OrderService/src/main/java/com/bbk/api/OrderService/controller/OrderConteroller.java
@@ -0,0 +1,5 @@
+package com.bbk.api.OrderService.controller;
+
+public class OrderConteroller {
+
+}
---
+++ OrderService/src/main/java/com/bbk/api/OrderService/service/OrderServiceApplication.java
@@ -0,0 +1,5 @@
+package com.bbk.api.OrderService.service;
+
+public class OrderServiceApplication {
+
+}
